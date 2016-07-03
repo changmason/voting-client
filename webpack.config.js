5f80531eb29b56244aab80ce208f2e231496c6d6
Mason Chang <changmason@gmail.com>
2016-07-03 15:16:51 +0800

    React and react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     './src/index.js'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'react-hot!babel'
     }, {
       test: /\.css$/,
       loader: 'style!css!autoprefixer?browsers=last 2 versions'
@@ -21,6 +23,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
